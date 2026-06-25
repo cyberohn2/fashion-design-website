34608b639d9662d4f303bee0e12b44075d7130a9
Edet John <edetjohn213@gmail.com>
2026-06-25 16:04:43 +0100

    fixed: broken images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
