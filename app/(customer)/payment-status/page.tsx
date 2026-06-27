fa681f6ca07f1d6471fa1db57ae0d5f06c097c3b
Edet John <edetjohn213@gmail.com>
2026-06-27 10:32:30 +0100

    still fixing: order and payment init oo omo

--- app/(customer)/payment-status/page.tsx
+++ app/(customer)/payment-status/page.tsx
@@ -13,6 +13,7 @@ const page = async ({searchParams}: {searchParams: Promise<{reference?: string;}
         return (<p>Payment Not found</p>)
     }
     const res = await paystack.get(`/transaction/verify/${paymentData.Provider_Reference}`);
+    console.log(paymentData.Provider_Reference, res.data)
     if (res.data.data.status === "success" && res.data.data.amount === paymentData.amount) {
         const updatePayment = await updatePaymentStatus({
         ref: paymentData.Provider_Reference,
