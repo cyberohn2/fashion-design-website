6f5df8e0cd6ab5fca0b531de5c30bbcadf201c3a
Edet John <edetjohn213@gmail.com>
2026-06-27 10:24:28 +0100

    still fixing: order and payment init oo omo

--- app/(customer)/payment-status/page.tsx
+++ app/(customer)/payment-status/page.tsx
@@ -1,22 +1,53 @@
+import { getPayment } from "@/actions/payments/get-payment";
+import { updatePaymentStatus } from "@/actions/payments/update-payment-status";
 import { PaymentStatusCard, PaymentStatusCardProp } from "@/components/app-components/payment/status";
+import { paystack } from "@/lib/paystack";
 
 const page = async ({searchParams}: {searchParams: Promise<{reference?: string;}>;}) => {
     const { reference } = await searchParams
+    if (!reference) {
+        return (<p>No Reference</p>)
+    }
+    const paymentData = await getPayment(reference)
+    if(!paymentData){
+        return (<p>Payment Not found</p>)
+    }
+    const res = await paystack.get(`/transaction/verify/${paymentData.Provider_Reference}`);
+    if (res.data.data.status === "success" && res.data.data.amount === paymentData.amount) {
+        const updatePayment = await updatePaymentStatus({
+        ref: paymentData.Provider_Reference,
+        status: "PAID",
+        });
+        return (
+            <main className="py-24 pt-34 md:pt-24 container mx-auto">
+                <div className="w-full max-w-sm md:max-w-4xl mx-auto">
+                    <PaymentStatusCard status={"success"} />
+                </div>
+            </main>
+        ); 
+    }
+    if(res.data.data.status === "failed"){
+        const updatePayment = await updatePaymentStatus({
+          ref: paymentData.Provider_Reference,
+          status: "FAILED",
+        });
+        return (
+          <main className="py-24 pt-34 md:pt-24 container mx-auto">
+            <div className="w-full max-w-sm md:max-w-4xl mx-auto">
+              <PaymentStatusCard status={"failed"} />
+            </div>
+          </main>
+        ); 
+    }
 
-    let status: PaymentStatusCardProp = "pending";
-    const req = await fetch(`/api/payment/verify?reference=${reference}`)
-    if(req.ok){
-        const data = await req.json()
-        status = data.data
-    } 
-
-  return (
+    return (
         <main className="py-24 pt-34 md:pt-24 container mx-auto">
             <div className="w-full max-w-sm md:max-w-4xl mx-auto">
-                <PaymentStatusCard status={status} />
+                <PaymentStatusCard status={"pending"} />
             </div>
         </main>
     );
+
 };
 
 export default page
