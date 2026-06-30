3a743143b22225852e8672818c4551e13a34ceff
Edet John <edetjohn213@gmail.com>
2026-06-30 16:41:05 +0100

    fixing: admin order details

--- components/admin-components/order/order-details.tsx
+++ components/admin-components/order/order-details.tsx
@@ -388,7 +388,13 @@ export function OrderDetails({ order }: OrderDetailsProps) {
             <Separator />
             <div>
               <p className="text-sm text-gray-600">Total Amount</p>
-              <p className="text-2xl font-bold">₦{order?.custom_order?.admin_final_price !== undefined ? order?.custom_order?.admin_final_price?.toFixed(2) : order?.total?.toFixed(2)}</p>
+              <p className="text-2xl font-bold">
+                ₦
+                {order?.custom_order?.admin_final_price === undefined ||
+                order?.custom_order?.admin_final_price === null
+                  ? order?.total?.toFixed(2)
+                  : order?.custom_order?.admin_final_price.toFixed(2)}
+              </p>
             </div>
             {order?.estimated_delivery && (
               <>
